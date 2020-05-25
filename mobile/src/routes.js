2f753ef55dbb35c1030e5ba6663fa12339f1e1a9
Renisson Silva <renissonx@gmail.com>
2020-05-25 18:43:29 -0300

    Personalizando header das telas

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -6,8 +6,25 @@ import Detail from './pages/Detail';
 
 const Routes = createAppContainer(
   createStackNavigator({
-    Main,
-    Detail,
+    Main:{
+      screen: Main,
+      navigationOptions:{
+        title:'Pokédex'
+      },
+    },
+    Detail:{
+      screen: Detail,
+      navigationOptions:{
+        title:'Pokémon'
+      }
+    },
+  },{
+    defaultNavigationOptions:{
+      headerTintColor:'#FFF',
+      headerStyle:{
+        backgroundColor:'#FF6F69',
+      }
+    }
   })
 );
 
