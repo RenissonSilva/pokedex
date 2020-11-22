a6c14cb8cce49d21d4b365a950dba7df3fe2750e
luanaflorencio <lua.m.016@gmail.com>
2020-11-22 16:17:26 -0300

    organização, modificações no código e realocação de arquivo

--- mobile/src/pages/Detail.js
+++ mobile/src/pages/Detail.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { View, Text, ImageBackground, TouchableOpacity, ScrollView, StatusBar, StyleSheet } from 'react-native';
-import api from '../services/api';
+import React from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import api from "./api";
 
-function Detail({ navigation }){
-
+function Detail({ navigation }) {
   // componentDidMount() {
   //   this.loadPokemons();
   // }
-  
+
   // loadPokemons = async () => {
   //   const reponse = await api.get('/api/v2/');
-  
+
   //   const { docs } = reponse.data;
-  
+
   //   console.log(docs);
   // };
-  return(
+  return (
     <ScrollView>
       <StatusBar barStyle="light-content" backgroundColor="#60CD8B" />
       <View style={styles.viewInfoPoke}>
-        <ImageBackground source={{ uri: "https://pokeres.bastionbot.org/images/pokemon/1.png" }} style={styles.image}></ImageBackground>
-        <Text style={styles.description}>Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.</Text>
+        <ImageBackground
+          source={{
+            uri: "https://pokeres.bastionbot.org/images/pokemon/1.png",
+          }}
+          style={styles.image}
+        ></ImageBackground>
+        <Text style={styles.description}>
+          Bulbasaur can be seen napping in bright sunlight. There is a seed on
+          its back. By soaking up the sun's rays, the seed grows progressively
+          larger.
+        </Text>
       </View>
       <View style={styles.viewStatusPoke}>
         <Text style={styles.title}>Stats</Text>
@@ -30,19 +46,31 @@ function Detail({ navigation }){
         <Text style={styles.title}>Type</Text>
 
         <View style={styles.divTypes}>
-          <TouchableOpacity style={[styles.btn, styles.grass]}><Text style={styles.name}>Grass</Text></TouchableOpacity>
-          <TouchableOpacity style={[styles.btn, styles.poison]}><Text style={styles.name}>Poison</Text></TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.grass]}>
+            <Text style={styles.name}>Grass</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.poison]}>
+            <Text style={styles.name}>Poison</Text>
+          </TouchableOpacity>
         </View>
 
         <Text style={styles.title}>Weaknesses</Text>
 
         <View style={styles.divTypes}>
-          <TouchableOpacity style={[styles.btn, styles.fire]}><Text style={styles.name}>Fire</Text></TouchableOpacity>
-          <TouchableOpacity style={[styles.btn, styles.psychic]}><Text style={styles.name}>Psychic</Text></TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.fire]}>
+            <Text style={styles.name}>Fire</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.psychic]}>
+            <Text style={styles.name}>Psychic</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.divTypes}>
-          <TouchableOpacity style={[styles.btn, styles.flying]}><Text style={[styles.name, styles.flying]}>Flying</Text></TouchableOpacity>
-          <TouchableOpacity style={[styles.btn, styles.ice]}><Text style={styles.name}>Ice</Text></TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.flying]}>
+            <Text style={[styles.name, styles.flying]}>Flying</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn, styles.ice]}>
+            <Text style={styles.name}>Ice</Text>
+          </TouchableOpacity>
         </View>
         <Text style={styles.title}>Abilities</Text>
         <Text style={styles.subTitle}>Overgrow</Text>
@@ -52,56 +80,56 @@ function Detail({ navigation }){
         ID: {JSON.stringify(navigation.getParam('pokeID'))}
       </Text> */}
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   image: {
     flex: 1,
-    width:180,
-    height:180,
+    width: 180,
+    height: 180,
     marginTop: 5,
   },
   viewInfoPoke: {
-    alignItems: 'center',
-    height:300,
-    backgroundColor: '#fff',
-    opacity:0.9,
+    alignItems: "center",
+    height: 300,
+    backgroundColor: "#fff",
+    opacity: 0.9,
   },
   description: {
-    fontSize : 16,
+    fontSize: 16,
     fontFamily: "monospace",
-    color: '#447D65',
-    margin:20,
-    fontWeight: 'bold',
-    opacity:0.7,
-    textAlign: 'justify',
+    color: "#447D65",
+    margin: 20,
+    fontWeight: "bold",
+    opacity: 0.7,
+    textAlign: "justify",
   },
   viewStatusPoke: {
-    backgroundColor: '#A4D5B7',
-    height: '100%',
+    backgroundColor: "#A4D5B7",
+    height: "100%",
     marginBottom: 30,
   },
-  title :{
-    color: '#fff',
+  title: {
+    color: "#fff",
     fontSize: 21,
     fontFamily: "monospace",
     marginHorizontal: 20,
     marginVertical: 15,
-    fontWeight: 'bold',
-    textShadowColor: 'rgba(0, 0, 0, 0.3)',
-    textShadowOffset: {width: -1, height: 1},
-    textShadowRadius: 10
+    fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.3)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
   },
   subTitle: {
-    color: '#fff',
+    color: "#fff",
     fontFamily: "monospace",
     marginLeft: 40,
     marginBottom: 10,
     fontSize: 17,
-    fontWeight: 'bold',
-    textShadowColor: 'rgba(0, 0, 0, 0.3)',
-    textShadowOffset: {width: -1, height: 1},
+    fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.3)",
+    textShadowOffset: { width: -1, height: 1 },
     textShadowRadius: 15,
   },
   btn: {
@@ -109,9 +137,9 @@ const styles = StyleSheet.create({
     height: 38,
     borderRadius: 12,
     borderWidth: 2,
-    borderColor: '#fff',
-    justifyContent: 'center',
-    fontWeight: 'bold',
+    borderColor: "#fff",
+    justifyContent: "center",
+    fontWeight: "bold",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -121,39 +149,38 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  name:{
-    color: '#fff',
-    textAlign: 'center',
+  name: {
+    color: "#fff",
+    textAlign: "center",
     fontFamily: "monospace",
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 18,
   },
   divTypes: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginHorizontal:25,
-    marginBottom:15,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginHorizontal: 25,
+    marginBottom: 15,
   },
   grass: {
-    backgroundColor: '#068f13',
-    
+    backgroundColor: "#068f13",
   },
   poison: {
-    backgroundColor: '#9364f5',
+    backgroundColor: "#9364f5",
   },
   fire: {
-    backgroundColor: '#ff7373',
+    backgroundColor: "#ff7373",
   },
   psychic: {
-    backgroundColor: '#e39dc7',
+    backgroundColor: "#e39dc7",
   },
   flying: {
-    backgroundColor: '#cff6ff',
-    color: '#757575',
+    backgroundColor: "#cff6ff",
+    color: "#757575",
   },
   ice: {
-    backgroundColor: '#4be3c8',
+    backgroundColor: "#4be3c8",
   },
-})
+});
 
-export default Detail; 
+export default Detail;
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,137 +1,218 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity,ImageBackground, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import api from '../services/api';
-function Main({ navigation }){
-  
-// componentDidMount(){
-//   this.loadPokemons();
-// }
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import api from "./api";
+function Main({ navigation }) {
+  // componentDidMount(){
+  //   this.loadPokemons();
+  // }
 
-// loadPokemons = async () => {
-//   const response = await api.get('/api/v2/');
+  // loadPokemons = async () => {
+  //   const response = await api.get('/api/v2/');
 
-//   const { docs } = response.data;
+  //   const { docs } = response.data;
 
-//   console.log(docs);
-// };
+  //   console.log(docs);
+  // };
   return (
     <>
       <View style={styles.searchForm}>
-        <TextInput style={styles.searchInput}
+        <TextInput
+          style={styles.searchInput}
           placeholder="Pesquise por nome"
           placeholderTextColor="#FFF"
           autoCapitalize="words"
           autoCorrect={false}
-          />
-          
+        />
       </View>
-      <TouchableOpacity onPress={() => navigation.navigate('Detail',{ pokeID: 1 })} style={[styles.cardPokemon , { backgroundColor:'#60CD8B', }]}>{/* Variavel no background de acordo com o tipo principal */}
-        <Text style={styles.titleCard}>Bulbasaur   #001</Text>{/* Variavel que pega nome e id do pokemon */}
-        <View style={styles.typeCards}>{/* Na hora que for listar lembrar de alterar ordem, pra o tipo principal aparecer primeiro */}
-            <Text style={[styles.typeText , { color:'#046D0E',borderColor:'#046D0E', }]}>Grass</Text>
-            <Text style={[styles.typeText , { color:'#794FD2',borderColor:'#794FD2', }]}>Poison</Text>{/* Colocar variavel pra colocar a cor de acordo com o tipo */}
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Detail", { pokeID: 1 })}
+        style={[styles.cardPokemon, { backgroundColor: "#60CD8B" }]}
+      >
+        {/* Variavel no background de acordo com o tipo principal */}
+        <Text style={styles.titleCard}>Bulbasaur #001</Text>
+        {/* Variavel que pega nome e id do pokemon */}
+        <View style={styles.typeCards}>
+          {/* Na hora que for listar lembrar de alterar ordem, pra o tipo principal aparecer primeiro */}
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#046D0E", borderColor: "#046D0E" },
+            ]}
+          >
+            Grass
+          </Text>
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#794FD2", borderColor: "#794FD2" },
+            ]}
+          >
+            Poison
+          </Text>
+          {/* Colocar variavel pra colocar a cor de acordo com o tipo */}
         </View>
-        <ImageBackground source={{ uri: "https://pokeres.bastionbot.org/images/pokemon/1.png" }} style={styles.image}>
-        </ImageBackground>
+        <ImageBackground
+          source={{
+            uri: "https://pokeres.bastionbot.org/images/pokemon/1.png",
+          }}
+          style={styles.image}
+        ></ImageBackground>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Detail',{ pokeID: 2 })} style={[styles.cardPokemon , { backgroundColor:'#FFAFAC', }]}>
-        <Text style={styles.titleCard}>Charmander   #004</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Detail", { pokeID: 2 })}
+        style={[styles.cardPokemon, { backgroundColor: "#FFAFAC" }]}
+      >
+        <Text style={styles.titleCard}>Charmander #004</Text>
         <View style={styles.typeCards}>
-            <Text style={[styles.typeText , { color:'#FF4949',borderColor:'#FF4949', }]}>Fire</Text>
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#FF4949", borderColor: "#FF4949" },
+            ]}
+          >
+            Fire
+          </Text>
         </View>
-        <ImageBackground source={{ uri: "https://pokeres.bastionbot.org/images/pokemon/4.png" }} style={styles.image}>
-        </ImageBackground>
+        <ImageBackground
+          source={{
+            uri: "https://pokeres.bastionbot.org/images/pokemon/4.png",
+          }}
+          style={styles.image}
+        ></ImageBackground>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Detail',{ pokeID: 3 })} style={[styles.cardPokemon , { backgroundColor:'#FFAFAC', }]}>
-        <Text style={styles.titleCard}>Charizard   #006</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Detail", { pokeID: 3 })}
+        style={[styles.cardPokemon, { backgroundColor: "#FFAFAC" }]}
+      >
+        <Text style={styles.titleCard}>Charizard #006</Text>
         <View style={styles.typeCards}>
-            <Text style={[styles.typeText , { color:'#FF4949',borderColor:'#FF4949', }]}>Fire</Text>
-            <Text style={[styles.typeText , { color:'#C8F5FF',borderColor:'#C8F5FF', }]}>Flying</Text>
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#FF4949", borderColor: "#FF4949" },
+            ]}
+          >
+            Fire
+          </Text>
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#C8F5FF", borderColor: "#C8F5FF" },
+            ]}
+          >
+            Flying
+          </Text>
         </View>
-        <ImageBackground source={{ uri: "https://pokeres.bastionbot.org/images/pokemon/6.png" }} style={styles.image}>
-        </ImageBackground>
+        <ImageBackground
+          source={{
+            uri: "https://pokeres.bastionbot.org/images/pokemon/6.png",
+          }}
+          style={styles.image}
+        ></ImageBackground>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigation.navigate('Detail',{ pokeID: 4 })} style={[styles.cardPokemon , { backgroundColor:'#96CDFF', }]}>
-        <Text style={styles.titleCard}>Squirtle   #007</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Detail", { pokeID: 4 })}
+        style={[styles.cardPokemon, { backgroundColor: "#96CDFF" }]}
+      >
+        <Text style={styles.titleCard}>Squirtle #007</Text>
         <View style={styles.typeCards}>
-            <Text style={[styles.typeText , { color:'#377EE8',borderColor:'#377EE8', }]}>Water</Text>
+          <Text
+            style={[
+              styles.typeText,
+              { color: "#377EE8", borderColor: "#377EE8" },
+            ]}
+          >
+            Water
+          </Text>
         </View>
-        <ImageBackground source={{ uri: "https://pokeres.bastionbot.org/images/pokemon/7.png" }} style={styles.image}>
-        </ImageBackground>
+        <ImageBackground
+          source={{
+            uri: "https://pokeres.bastionbot.org/images/pokemon/7.png",
+          }}
+          style={styles.image}
+        ></ImageBackground>
       </TouchableOpacity>
     </>
-  )}
+  );
+}
 
-  const styles = StyleSheet.create({
-    searchForm:{
-      position:'absolute',
-      top:20,
-      left:40,
-      right:40,
-      flexDirection:'row',
+const styles = StyleSheet.create({
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 40,
+    right: 40,
+    flexDirection: "row",
+  },
+  image: {
+    flex: 1,
+    opacity: 0.4,
+    width: 120,
+    marginLeft: 20,
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFAFAC",
+    color: "#FFF",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    fontFamily: "monospace",
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
     },
-    image: {
-      flex: 1,
-      opacity:0.4,
-      width:120,
-      marginLeft:20,
-    },
-    searchInput:{
-      flex:1,
-      height:50,
-      backgroundColor:'#FFAFAC',
-      color:'#FFF',
-      borderRadius:25,
-      paddingHorizontal:20,
-      fontSize:16,
-      fontFamily: "monospace",
-      shadowColor:'#000',
-      shadowOpacity:0.2,
-      shadowOffset:{
-        width:4,
-        height:4,
-      },
-      elevation:2,
-    },
+    elevation: 2,
+  },
 
-    cardPokemon:{
-      top:100,
-      height:85,
-      marginBottom:15,
-      borderRadius:15,
-      marginHorizontal:20,
-    },
+  cardPokemon: {
+    top: 100,
+    height: 85,
+    marginBottom: 15,
+    borderRadius: 15,
+    marginHorizontal: 20,
+  },
 
-    titleCard:{
-      color:'#FFF',
-      fontSize:17,
-      position:'absolute',
-      right:20,
-      top:5,
-      fontFamily: "monospace",
-      fontWeight:'bold',
-    },
+  titleCard: {
+    color: "#FFF",
+    fontSize: 17,
+    position: "absolute",
+    right: 20,
+    top: 5,
+    fontFamily: "monospace",
+    fontWeight: "bold",
+  },
 
-    typeText:{
-      fontSize:15,
-      fontFamily: "monospace",
-      fontWeight:'bold',
-      borderRadius:15,
-      paddingHorizontal:25,
-      paddingVertical:2,
-      marginLeft:5,
-      borderWidth:1,
-    },
-    typeCards:{
-      flexDirection:'row',
-      position:'absolute',
-      right:10,
-      bottom:10,
-    }
-  })
+  typeText: {
+    fontSize: 15,
+    fontFamily: "monospace",
+    fontWeight: "bold",
+    borderRadius: 15,
+    paddingHorizontal: 25,
+    paddingVertical: 2,
+    marginLeft: 5,
+    borderWidth: 1,
+  },
+  typeCards: {
+    flexDirection: "row",
+    position: "absolute",
+    right: 10,
+    bottom: 10,
+  },
+});
 
-export default Main; 
+export default Main;
